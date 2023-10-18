c949b418e35044c33613bb1c378e4414e2e4e8ad
canitry <56244521+canitry@users.noreply.github.com>
2023-10-18 10:25:05 -0700

    add my work

--- Cdecrypt.c
+++ Cdecrypt.c
@@ -1,6 +1,7 @@
 // version 1.0 11/12/2022
 #include <stdio.h>
 #include "cipher.h"
+#define NBITS 8
 
 #ifdef MYDECRYPT_C
 TODO("using Cdecrypt.c");
@@ -9,7 +10,32 @@ int
 decrypt(char *iobuf, char *bookbuf, int cnt)
 {
 
-    // your code here 
+    // your code here
+    unsigned int byte; /* copies encrypted bytes in iobuf to decode */
+    unsigned int decByte; /* the decrypted byte */
+    unsigned int mask = 1; /* mask to isolate each bit to un-reverse it */
+    unsigned int masked; /* isolated bit to be unreversed */
+    unsigned int bkByte; /* copy of coresp byte of book buf to var byte */
+   /*
+    * traverses iobuf till all bytes in buffer are encrypted
+    */
+    for (int j = 0; j < cnt; j++){
+        byte = *(iobuf + j);
+        /* eors with book cipher */
+        bkByte = *(bookbuf + j);
+        byte = byte ^ bkByte;
+        /* unreverses the byte */
+        decByte = 0;
+        for (int i = 0; i < NBITS; i++){
+            masked = byte & mask;
+            byte = byte >> 1;
+            masked = masked << (NBITS-1-i);
+            decByte = decByte + masked;
+        }
+
+        /* replaces the byte in buffer with the decrypted byte */
+        *(iobuf+j)=decByte;
+    }
 
     return cnt;
 }
--- Cencrypt.c
+++ Cencrypt.c
@@ -1,7 +1,7 @@
 // version 1.0 11/12/2022
 #include <stdio.h>
 #include "cipher.h"
-
+#define NBITS 8
 #ifdef MYENCRYPT_C
 TODO("using Cencrypt.c");
 
@@ -10,6 +10,30 @@ encrypt(char *iobuf, char *bookbuf, int cnt)
 {
 
     // your code here
+    unsigned int byte; /* copy of iobuf bytes */
+    unsigned int revByte; /* the var byte reversed */
+    unsigned int mask = 1; /* mask to isolate each bit to reverse it */
+    unsigned int masked; /* holds isolated bit that will be reversed */
+    unsigned int bkByte; /* copies byte of bookbuf to encrypt byte */
+   /*
+    * traverses iobuf till all bytes in buffer are encrypted
+    */
+    for (int j = 0; j < cnt; j++){
+        /* reverses the bits in the byte*/
+        byte = *(iobuf + j);
+        revByte = 0;
+        for (int i = 0; i < NBITS; i++){
+            masked = byte & mask;
+            byte = byte >> 1;
+            masked = masked << (NBITS-1-i);
+            revByte = revByte + masked;
+        }
+        /* eors with book cipher */
+        bkByte = *(bookbuf + j);
+        revByte = revByte ^ bkByte;
+        /* replaces byte in buffer with encrypted byte */
+        *(iobuf+j)=revByte;
+    }
 
     return cnt;
 }
